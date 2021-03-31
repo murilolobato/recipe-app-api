e11b5eeee72f7f2c77abc3f8ceb587f3790b51d8
Murilo Lobato <murilolobatto@gmail.com>
2021-03-30 21:42:05 -0300

    Updated admin to support custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
